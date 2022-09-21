2d70807fb8f1ebfbe40402803ef513334a14d847
britesc <julian@britesc.com>
2022-09-21 13:25:30 +0200

    Add files via upload
    Only way I could get the files onto GitHub

---
+++ Build/CMakeFiles/Tuxedo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tuxedo.dir/main.c.o"
+  "CMakeFiles/Tuxedo.dir/main.c.o.d"
+  "Tuxedo"
+  "Tuxedo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tuxedo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Source/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>"
+#include "../Include/main.h"
+
+int main() {
+	printf("Hello, World!");
+	return 0;"
+}
